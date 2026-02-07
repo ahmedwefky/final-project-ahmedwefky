2c7797e4edbae4eecd1dea211f0e834a3ab792fe
Ahmed Wefky <awefky@icloud.com>
2026-02-07 19:59:50 +0100

    Updated build script

--- build.sh
+++ build.sh
@@ -10,21 +10,25 @@ git submodule update --recursive
 
 if [ ! -e buildroot/.config ]
 then
-	echo "MISSING BUILDROOT CONFIGURATION FILE"
+	echo "\nMISSING BUILDROOT CONFIGURATION FILE\n"
 
-	if [ -e ../buildroot_external/configs/rpi4_buildroot_config ]
+	if [ -e buildroot_external/configs/rpi4_buildroot_config ]
 	then
-		echo "USING rpi4_buildroot_config"
+		echo "\nUsing existing rpi4_buildroot_config\n"
 		make -C buildroot defconfig BR2_EXTERNAL=../buildroot_external BR2_DEFCONFIG=../buildroot_external/configs/rpi4_buildroot_config
+		echo "\nRun this script again to build the image\n"
 	else
 		echo "Run ./save_config.sh to save this as the default configuration"
 		echo "Then add packages as needed to complete the installation, re-running ./save_config.sh as needed"
 		make -C buildroot defconfig BR2_EXTERNAL=../buildroot_external BR2_DEFCONFIG=../buildroot_external/configs/rpi4_buildroot_config
 	fi
 else
-	echo "USING EXISTING BUILDROOT CONFIG"
+	echo "\nUSING EXISTING BUILDROOT CONFIG\n"
+	START_TIME=$(date +%s)
 	make -C buildroot BR2_EXTERNAL=../buildroot_external
+	END_TIME=$(date +%s)
+	ELAPSED=$((END_TIME - START_TIME))
+	echo "\nBuild complete."
+	echo "\nBuild took $(($ELAPSED / 60))m $(($ELAPSED % 60))s"
+	echo "\nImage available in buildroot/output/images/"
 fi
-
-echo "Build complete."
-echo "Images available in buildroot/output/images/"
